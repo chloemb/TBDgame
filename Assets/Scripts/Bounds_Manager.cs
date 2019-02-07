315dd6a73bafe461163b58402c3e4c861fb0af77
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2019-02-06 20:29:23 -0600

    Starting basics
    Just created a basic scene using tilemaps and created a rudimentiary character. Also added a bounds manager

---
+++ Assets/Scripts/Bounds_Manager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds_Manager : MonoBehaviour
+{
+
+    private static Vector4 _bounds;
+
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+        internal void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+
+            var cam = Camera.main;
+            var bottomleft = cam.ViewportToWorldPoint(Vector3.zero);
+            var topright = cam.ViewportToWorldPoint(new Vector3(1f, 1f, 0f));
+
+            _bounds.x = bottomleft.x;
+            _bounds.y = bottomleft.y;
+            _bounds.z = topright.x;
+            _bounds.w = topright.y;
+        }
+
+    // Update is called once per frame
+        internal void Update()
+        {
+            var pos = _rb.position;
+
+            if (pos.x < _bounds.x)
+            {
+                pos.x = _bounds.z;
+            }
+            else if (pos.x > _bounds.z)
+            {
+                pos.x = _bounds.x;
+            }
+
+            if (pos.y < _bounds.y)
+            {
+                pos.y = _bounds.w;
+            }
+            else if (pos.y > _bounds.w)
+            {
+                pos.y = _bounds.y;
+            }
+
+            _rb.position = pos;
+        }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float Speed;
+    public float JumpHeight;
+    private bool IsGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        IsGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        { 
+            Vector2 movement = new Vector2(Speed * horizontal, JumpHeight);
+            _rb.velocity = new Vector2(0, 0);
+            _rb.AddForce(movement, ForceMode2D.Impulse);
+            IsGrounded = false;
+        }
+        else
+        {
+            _rb.velocity = new Vector2(Speed * horizontal, _rb.velocity.y);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Surface")
+        {
+            IsGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Surface")
+        {
+            IsGrounded = false;
+        }
+    }
+}
