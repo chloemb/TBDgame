9cc98892bee4c570eec9b1a9feff53b396465e7b
Chloe <chloebrown2020@u.northwestern.edu>
2019-02-08 15:35:05 -0600

    adjusted jumping mechanics
    can now wall jump + other small qol adjustments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,13 @@ public class PlayerController : MonoBehaviour
     public float Speed;
     public float JumpHeight;
 
-    public bool IsGrounded;
-    public bool StuckOnWall;
-    public bool IgnoreHorizontal;
+    private bool IsGrounded;
+    private bool TouchWallToRight;
+    private bool TouchWallToLeft;
+    private bool IgnoreLeft;
+    private bool IgnoreRight;
+    private Vector2 _prevVelocity;
+    private bool UsedWallJump;
 
     public string[] PlayerAxes;
 
@@ -35,6 +39,7 @@ public class PlayerController : MonoBehaviour
         }
 
         IsGrounded = false;
+        _prevVelocity = new Vector2(1, 1);
     }
 
     // Update is called once per frame
@@ -48,28 +53,43 @@ public class PlayerController : MonoBehaviour
             if (jump > 0)
             {
                 Vector2 errorVector2 = new Vector2(Speed * horizontal, JumpHeight) - new Vector2(_rb.velocity.x, 0);
-                // _rb.velocity = new Vector2(0, 0);
                 _rb.AddForce(errorVector2, ForceMode2D.Impulse);
                 IsGrounded = false;
             }
         }
 
-        if (StuckOnWall)
+        if (TouchWallToLeft || TouchWallToRight)
         {
-            if (jump > 0)
+            if (jump > 0 && !UsedWallJump)
             {
                 Vector2 errorVector2 = new Vector2(0, JumpHeight);
+                _rb.velocity = new Vector2(0, 0);
                 _rb.AddForce(errorVector2, ForceMode2D.Impulse);
-                
-                IgnoreHorizontal = true;
-                StuckOnWall = false;
+
+                if (TouchWallToLeft)
+                {
+                    IgnoreLeft = true;
+                }
+                else
+                {
+                    IgnoreRight = true;
+                }
+
+                UsedWallJump = true;
             }
         }
 
-        // _rb.velocity = new Vector2(Speed * horizontal, _rb.velocity.y);
-        if (!IgnoreHorizontal)
+        Vector2 forcetoapply = new Vector2(horizontal * Speed, 0) - new Vector2(_rb.velocity.x, 0);
+        if ((IgnoreLeft && forcetoapply.x < 0) || (IgnoreRight && forcetoapply.x > 0))
         {
-            _rb.AddForce(new Vector2(horizontal * Speed, 0) - new Vector2(_rb.velocity.x, 0), ForceMode2D.Impulse);
+            forcetoapply.x = 0;
+        }
+
+        _rb.AddForce(forcetoapply, ForceMode2D.Impulse);
+
+        if (_rb.velocity != Vector2.zero)
+        {
+            _prevVelocity = _rb.velocity;
         }
     }
 
@@ -83,7 +103,14 @@ public class PlayerController : MonoBehaviour
 
         if (col.gameObject.tag == "Walls")
         {
-            StuckOnWall = true;
+            if (_prevVelocity.x < 0)
+            {
+                TouchWallToLeft = true;
+            }
+            else
+            {
+                TouchWallToRight = true;
+            }
         }
     }
 
@@ -96,8 +123,11 @@ public class PlayerController : MonoBehaviour
 
         if (col.gameObject.tag == "Walls")
         {
-            IgnoreHorizontal = false;
-            StuckOnWall = false;
+            TouchWallToLeft = false;
+            TouchWallToRight = false;
+            IgnoreLeft = false;
+            IgnoreRight = false;
+            UsedWallJump = false;
         }
     }
 }
