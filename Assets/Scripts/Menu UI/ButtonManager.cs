c3d4048bb6912d08fdf448a224b8c7a6b218a10b
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2019-02-14 15:41:42 -0600

    Finished up Start Menu
    Gave the menu functionality and spent way too long fiddling with buttons

---
+++ Assets/Scripts/Menu UI/ButtonManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public Image fade;
+
+    private float _fadeDelay = 1f;
+
+    public void Start()
+    {
+        var startButton = transform.Find("Buttons/Start").gameObject.GetComponent<Button>();
+        startButton.onClick.AddListener(() => StartCoroutine(StartFade()));
+
+        var quitButton = transform.Find("Buttons/Quit").gameObject.GetComponent<Button>();
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private IEnumerator StartFade()
+    {
+        var color = Color.black;
+        while (_fadeDelay > 0f)
+        {
+            _fadeDelay -= .5f * Time.deltaTime;
+            
+            color.a = 1f - _fadeDelay;
+            fade.color = color;
+            yield return null;
+        }
+
+        StartGame();
+    }
+
+    private static void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private static void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
---
+++ Assets/Scripts/Menu UI/MenuButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text theText;
+
+    private Color original;
+
+    public void Start()
+    {
+        original = theText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = new Color(255, 255, 255, 255); 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = original;
+    }
+}
