39f05352ab70b9c64d2aaccf565d279a1d82f550
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2019-02-14 01:58:22 -0600

    Marginal Edits to Menu

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void onClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Environment Experiments");
     }
 }
