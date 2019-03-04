1be7beb47919c08aa8bf93059623ebb038ec3f51
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2019-03-04 13:25:37 -0600

    General UI Updates, updated scenes and scores,
    Also fucked around with moving platforms for a long time, but I couldn't figure out how to fix it easily

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -22,22 +22,22 @@ public class SceneSwitch : MonoBehaviour
         Music = GameObject.Find("GlobalControl").GetComponent<AudioSource>();
         Music.volume = 1f;
         Music.Play();
-        _kills1Happened = 0;
-        _kills2Happened = 0;
+        _kills1Happened = KillsRequired;
+        _kills2Happened = KillsRequired;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Score1.text = _kills2Happened.ToString();
-        Score2.text = _kills1Happened.ToString();
-        if (_kills1Happened >= KillsRequired)
+        Score1.text = _kills1Happened.ToString();
+        Score2.text = _kills2Happened.ToString();
+        if (_kills1Happened <= 0)
         {
             //End to quit game screen
             SceneManager.LoadScene(4);
             SaveData("Player2");
         }
-        else if (_kills2Happened >= KillsRequired)
+        else if (_kills2Happened <= 0)
         {
             SceneManager.LoadScene(4);
             SaveData("Player1");
@@ -46,12 +46,12 @@ public class SceneSwitch : MonoBehaviour
 
     void Player1Killed()
     {
-        _kills1Happened++;
+        _kills1Happened--;
     }
 
     void Player2Killed()
     {
-        _kills2Happened++;
+        _kills2Happened--;
     }
 
     void SaveData(string player)
