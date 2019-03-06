109a92782af475590604b1b024f2dcf33b18184a
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2019-03-06 17:56:37 -0600

    Master-test

--- Assets/Scripts/Player/PlayerRespawner.cs
+++ Assets/Scripts/Player/PlayerRespawner.cs
@@ -7,10 +7,16 @@ public class PlayerRespawner : MonoBehaviour
     public List<Transform> PlayerSpawnPoints;
     public GameObject[] Players;
     public GameObject MovingPlatforms;
+    private bool _mPlatform;
 
     // Start is called before the first frame update
     void Start()
     {
+        _mPlatform = true;
+        if (!FindObjectOfType<MovingPlatform>())
+        {
+            _mPlatform = false;
+        }
         foreach (var child in GetComponentsInChildren<Transform>())
         {
             if (child.gameObject.name.Contains("Respawn"))
@@ -23,14 +29,30 @@ public class PlayerRespawner : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (PlayerSpawnPoints[0].childCount == 0  &&  MovingPlatforms.transform.childCount == 0)
+        if (_mPlatform)
         {
-            Instantiate(Players[0], PlayerSpawnPoints[0]);
+            if (PlayerSpawnPoints[0].childCount == 0 && MovingPlatforms.transform.childCount == 0)
+            {
+                Instantiate(Players[0], PlayerSpawnPoints[0]);
+            }
+
+            if (PlayerSpawnPoints[1].childCount == 0 && MovingPlatforms.transform.childCount == 0)
+            {
+                Instantiate(Players[1], PlayerSpawnPoints[1]);
+            }
+        }
+        else
+        {
+            if (PlayerSpawnPoints[0].childCount == 0)
+            {
+                Instantiate(Players[0], PlayerSpawnPoints[0]);
+            }
+
+            if (PlayerSpawnPoints[1].childCount == 0)
+            {
+                Instantiate(Players[1], PlayerSpawnPoints[1]);
+            }
         }
         
-        if (PlayerSpawnPoints[1].childCount == 0 && MovingPlatforms.transform.childCount == 0)
-        {
-            Instantiate(Players[1], PlayerSpawnPoints[1]);
-        }
     }
 }
